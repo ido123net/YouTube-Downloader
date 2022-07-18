47bf86e729fcc1cedb8c317680f21c9b513fc7fb
Ido Frenkel <Ido123net@gmail.com>
2022-07-18 09:00:52 +0300

    add playlist argument

--- youtube-downloader/__main__.py
+++ youtube-downloader/__main__.py
@@ -5,6 +5,7 @@ import logging
 
 from .convert import convert_to_mp3
 from .download import download_urls
+from .playlist import get_playlist_urls
 
 logging.basicConfig(
     level=logging.INFO,
@@ -26,6 +27,12 @@ def arg_parse():
         default=None,
     )
     parser.add_argument(
+        "-p",
+        "--playlist-url",
+        type=str,
+        default=None,
+    )
+    parser.add_argument(
         "--mp3",
         action="store_true",
     )
@@ -41,16 +48,21 @@ def arg_parse():
 
 def main():
     args = arg_parse()
-    try:
-        with open(args.file) as f:
-            f_urls = f.read().splitlines()
-    except TypeError:
-        f_urls = []
+    f_urls = urls_from_file(args.file)
+    p_urls = get_playlist_urls(args.playlist_url)
 
-    urls = f_urls + args.urls
+    urls = f_urls + args.urls + p_urls
     videos = download_urls(urls=urls, output_path=args.output_path)
     if args.mp3:
         convert_to_mp3(videos=videos, output_path=args.output_path)
+
+def urls_from_file(urls_file):
+    try:
+        with open(urls_file) as f:
+            f_urls = f.read().splitlines()
+    except TypeError:
+        f_urls = []
+    return f_urls
 
 
 if __name__ == "__main__":
