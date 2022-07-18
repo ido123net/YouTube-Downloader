28d3aab753f3c6167496b4d9eb9e6b4df08c058e
Ido Frenkel <Ido123net@gmail.com>
2022-07-18 09:00:38 +0300

    fix missing folder

--- youtube-downloader/convert.py
+++ youtube-downloader/convert.py
@@ -7,9 +7,11 @@ logger = logging.getLogger(__name__)
 
 
 def convert_to_mp3(videos: list[str], output_path: str = "out"):
+    output_folder = os.path.join(output_path, "audio")
+    os.makedirs(output_folder, exist_ok=True)
     for video in videos:
         logger.info(f"converting {video} to mp3")
         filename = os.path.splitext(os.path.split(video)[1])[0]
         VideoFileClip(video).audio.write_audiofile(
-            os.path.join(output_path, "audio", f"{filename}.mp3")
+            os.path.join(output_folder, f"{filename}.mp3")
         )
